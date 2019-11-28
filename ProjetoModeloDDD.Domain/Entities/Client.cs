e942461d24e425b9eaccae52c3778182c39fbdf2
luiz Alex <luizalex1997@gmail.com>
2019-11-28 14:18:18 -0300

    Add Structure

---
+++ ProjetoModeloDDD.Domain/Entities/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoModeloDDD.Domain.Entities
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+        public bool Active { get; set; }
+
+        public bool PremmiumClient(Client client)
+        {
+            var ticksPassed = (DateTime.Now - client.RegisterDate.AddYears(5)).Ticks;
+            return client.Active && (ticksPassed > 0);
+        }
+    }
+}
